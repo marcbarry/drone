c017873314ad98d6244b2a1234b7834b1c45cba6
marc barry <67782240+enclave-marc-barry@users.noreply.github.com>
2021-03-14 17:45:21 +0000

    i2c skeleton

--- mpu9250/Program.cs
+++ mpu9250/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Device.I2c;
+using System.IO;
 
 namespace mpu9250
 {
-    class Program
+    /// <summary>
+    /// https://github.com/dotnet/iot/blob/main/Documentation/raspi-i2c.md
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var busId = 1;
+            var deviceAddress = 0x12;
+            var validAddresses = new List<int>();
+
+            // First 8 I2C addresses are reserved, last one is 0x7F
+            for (var i = 8; i < 0x80; i++)
+            {
+                try
+                {
+                    var i2c = I2cDevice.Create(new I2cConnectionSettings(busId, i));
+                    var read = i2c.ReadByte();
+                    validAddresses.Add(i);
+                }
+                catch (IOException)
+                {
+                    // Do nothing, there is just no device
+                }
+            }
+
+
+            foreach (var valid in validAddresses)
+            {
+                Console.WriteLine($"Address: 0x{valid:X}");
+            }
+
+            Console.ReadLine();
+
+            using (var i2c = I2cDevice.Create(new I2cConnectionSettings(busId, deviceAddress)))
+            {
+                i2c.WriteByte(0x42);
+                var read = i2c.ReadByte();
+            }
         }
     }
 }
